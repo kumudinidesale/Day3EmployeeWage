f75a8106f2ae0e0b3e7d1a06167d12df2a5d4a22
kumudinidesale <kdesale1211@gmail.com>
2022-02-12 20:47:36 +0530

    added:Employe is Absent or Present

---
+++ src/com/java/EmployeeWageComputation.java
@@ -0,0 +1,17 @@
+package com.java;
+
+public class EmployeeWageComputation {
+
+	public static void main(String[] args) {
+		int isFULL_TIME = 1;
+		double empcheck = (int) (Math.random() * 100) % 2;
+		if (empcheck == isFULL_TIME)
+
+			System.out.println("Employee is Present");
+
+		else
+
+			System.out.println("Employee is Absent");
+
+	}
+}
