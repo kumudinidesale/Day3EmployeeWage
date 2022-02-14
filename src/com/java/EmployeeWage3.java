1b544f124718862071851335ebf28049821cd502
kumudinidesale <97426875+kumudinidesale@users.noreply.github.com>
2022-02-14 13:24:21 +0530

    Update EmployeeWage3.java

--- src/com/java/EmployeeWage3.java
+++ src/com/java/EmployeeWage3.java
@@ -3,36 +3,37 @@ package com.java;
 import java.util.Random;
 
 public class EmployeeWage3 {
+	public class EmployeeWageComputation {
 	static final int IS_FULL_TIME = 1;
-	static final int IS_PART_TIME = 2;
 	static final int EMP_RATE_PER_HR = 20;
-
+	
 	public static int empCheckStatus() {
 		int empWrkHrs = 0;
 		Random random = new Random();
-		int empCheck = random.nextInt(3);
-		if (empCheck == IS_FULL_TIME) {
+		int empCheck = random.nextInt(2);
+		if(empCheck == IS_FULL_TIME) {
+			System.out.println("Employee is Present");
 			empWrkHrs = 8;
-		} else if (empCheck == IS_PART_TIME) {
-			empWrkHrs = 4;
-		} else {
+		}
+		else {
+			System.out.println("Employee is absent");
 			empWrkHrs = 0;
 		}
 		return empWrkHrs;
 	}
-
+	
 	public static int empWagePerDayStatus(int empWrkHrs) {
 		int empWagePerDay = empWrkHrs * EMP_RATE_PER_HR;
 		return empWagePerDay;
 	}
 
-	public static void main(String[] args) {
+	public static void main(String[] args) {	
 		int empWrkHrs;
 		int empWagePerDay;
 		empWrkHrs = empCheckStatus();
 		empWagePerDay = empWagePerDayStatus(empWrkHrs);
-		System.out.println("Employee Wage per day: " + empWagePerDay);
-
+		System.out.println("Employee Wage per day: "+empWagePerDay);
+		
 	}
 
 }
