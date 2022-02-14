e2553952d213bfcb61fd114c1c1c0285e847d715
kumudinidesale <97426875+kumudinidesale@users.noreply.github.com>
2022-02-14 12:52:15 +0530

    Update EmployeeWageComputation.java

--- src/com/java/EmployeeWageComputation.java
+++ src/com/java/EmployeeWageComputation.java
@@ -3,9 +3,9 @@ package com.java;
 public class EmployeeWageComputation {
 
 	public static void main(String[] args) {
-		int isFULL_TIME = 1;
+		int PRESENT = 1;
 		double empcheck = (int) (Math.random() * 100) % 2;
-		if (empcheck == isFULL_TIME)
+		if (empcheck == PRESENT)
 
 			System.out.println("Employee is Present");
 
